fa15c97e2b8b9517f0e77b2e723cec6ad00dd527
Sergey Bugaev <bugaevc@gmail.com>
2024-08-04 22:19:01 +0300

    Bits of Variant/DBus hacking

--- include/peel/GLib/Variant.h
+++ include/peel/GLib/Variant.h
@@ -14,10 +14,58 @@ class VariantIter;
 
 class Variant
 {
+private:
+  Variant () = delete;
+  Variant (const Variant &) = delete;
+  Variant (Variant &&) = delete;
+  Variant &
+  operator = (const Variant &) = delete;
+  Variant &
+  operator = (Variant &&) = delete;
+
 public:
   class Type;
   struct Builder;
   typedef VariantIter Iter;
+
+  peel_nothrow
+  const Type *
+  get_type ()
+  {
+    ::GVariant *_peel_this = reinterpret_cast<::GVariant *> (this);
+    const ::GVariantType *_peel_return = g_variant_get_type (_peel_this);
+    return reinterpret_cast<const Type *> (_peel_return);
+  }
+
+  peel_nothrow
+  const char *
+  get_type_string ()
+  {
+    ::GVariant *_peel_this = reinterpret_cast<::GVariant *> (this);
+    return g_variant_get_type (_peel_this);
+  }
+
+  peel_nothrow
+  bool
+  is_of_type (const GVariantType *type)
+  {
+    ::GVariant *_peel_this = reinterpret_cast<::GVariant *> (this);
+    const ::GVariantType *_peel_type = reinterpret_cast<const ::GVariantType *> (type);
+    gboolean _peel_return = g_variant_is_of_type (_peel_this, _peel_type);
+    return !!_peel_return;
+  }
+
+  // ...
+
+  template<typename ...Args>
+  peel_nothrow
+  static peel::FloatPtr<Variant>
+  create (const char *format_string, Args... args)
+  {
+    // TODO: this should handle floating refs properly
+    ::GVariant *_peel_return = g_variant_new (format_string, args...);
+    return peel::FloatPtr<Variant> (reinterpret_cast<Variant *> (_peel_return));
+  }
 };
 
 } /* namespace GLib */
