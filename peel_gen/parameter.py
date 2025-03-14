6a72a215767d0bed061251715d8b9ae66841dfc4
Sergey Bugaev <bugaevc@gmail.com>
2025-03-14 21:44:26 +0300

    Drop the caller-allocates non-allocatable error
    This can happen, see Pango::GlyphString. Indeed, we don't have to allocate on the stack.

--- peel_gen/parameter.py
+++ peel_gen/parameter.py
@@ -205,8 +205,6 @@ class Parameter(NodeHandler):
         if self.direction == 'in' or for_local_copy:
             out_asterisk = ''
         elif self.caller_allocates and tp.is_passed_by_ref():
-            if not tp.can_be_allocated_by_value():
-                raise UnsupportedForNowException('caller-allocates non-allocatable type (perhaps {} should be marked onstack?)'.format(tp))
             # Don't add an additional asterisk to the one that will be added
             # by the usual "this type gets passed by ref" logic anyway.
             out_asterisk = ''
