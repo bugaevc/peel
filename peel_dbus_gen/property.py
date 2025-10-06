08f6790b863414c4a4ceba0a052683f804c1c1e2
Sergey Bugaev <bugaevc@gmail.com>
2025-10-06 18:56:27 +0300

    D-Bus hacking

--- peel_dbus_gen/property.py
+++ peel_dbus_gen/property.py
@@ -69,14 +69,32 @@ class Property:
                 '  ::GVariant *v = g_dbus_proxy_get_cached_property (_peel_self, "{}");'.format (self.dbus_name),
                 '  if (v == nullptr)',
                 '    {',
+                '      ::GError *error = nullptr;',
                 '      ::GVariant *reply = g_dbus_proxy_call_sync (',
                 '        _peel_self, "org.freedesktop.DBus.Properties.Get",',
                 '        g_variant_new ("(ss)", "{}", "{}"),'.format(self.iface.interface_name, self.dbus_name),
-                '        G_DBUS_CALL_FLAGS_NONE, -1, nullptr, nullptr);',
-                '      if (reply == nullptr)',
-                '        return;',
+                '        G_DBUS_CALL_FLAGS_NONE, -1, nullptr, &error);',
+                '      if (G_UNLIKELY (reply == nullptr))',
+                '        {',
+                '          g_warning ("Failed to get property %s: %s", "{}", error->message);'.format(self.dbus_name),
+                '          g_error_free (error);',
+                '          return;',
+                '        }',
+                '      if (G_UNLIKELY (!g_variant_is_of_type (reply, G_VARIANT_TYPE ("(v)"))))',
+                '        {',
+                '          g_warning ("Received unexpected reply type for org.freedesktop.DBus.Properties.Get");',
+                '          g_variant_unref (reply);',
+                '          return;',
+                '        }',
                 '      g_variant_get_child (reply, 0, "v", &v);',
                 '      g_variant_unref (reply);',
+                '      const char *tp = g_variant_get_type_string (v);',
+                '      if (G_UNLIKELY (strcmp (tp, "{}")))'.format(self.type.signature),
+                '        {',
+                '          g_warning ("Received value of type %s for property %s, but expected type %s", tp, "{}", "{}");'.format(self.dbus_name, self.type.signature),
+                '          g_variant_unref (v);',
+                '          return;',
+                '        }',
                 '    }',
                 '  {};'.format(self.type.generate_set_value_from_variant(value_expr='value', variant_expr='v')),
                 '  g_variant_unref (v);',
@@ -115,3 +133,26 @@ class Property:
 
     def generate_proxy_override_pspec(self, class_expr):
         return '  g_object_class_override_property ({}, {}, "{}");'.format(class_expr, self.generate_enum_member(), self.prop_name)
+
+    def generate_property_info(self):
+        name = '{}_{}_property_info'.format(self.iface.emit_name, self.cpp_name)
+        if self.access == 'readwrite':
+            flags = 'G_DBUS_PROPERTY_INFO_FLAGS_READABLE | G_DBUS_PROPERTY_INFO_FLAGS_WRITABLE'
+        elif self.access == 'read':
+            flags = 'G_DBUS_PROPERTY_INFO_FLAGS_READABLE'
+        elif self.access == 'write':
+            flags = 'G_DBUS_PROPERTY_INFO_FLAGS_WRITABLE'
+        else:
+            flags = 'G_DBUS_PROPERTY_INFO_FLAGS_NONE'
+        l = [
+            'static const ::GDBusPropertyInfo',
+            '{} ='.format(name),
+            '{',
+            '  -1, /* ref_count */',
+            '  const_cast<char *> ("{}"),'.format(self.dbus_name),
+            '  const_cast<char *> ("{}"),'.format(self.type.signature),
+            '  ::GDBusPropertyInfoFlags ({}),'.format(flags),
+            '  nullptr /* annotations */',
+            '};',
+        ]
+        return name, '\n'.join(l)
